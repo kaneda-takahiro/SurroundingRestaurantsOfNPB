fe1871e31876e1791b00878ef34dca64923e3575
金田岳大 <kanedtakahiro@kanedatakeshidainoMacBook-Air.local>
2022-07-14 21:19:36 +0900

    PRの修正

--- SurroundingRestaurantsOfNPB/Controller/restaurantInfoViewController.swift
+++ SurroundingRestaurantsOfNPB/Controller/restaurantInfoViewController.swift
@@ -14,16 +14,5 @@ class restaurantInfoViewController: UITableViewController {
     }
 
     private func setupTableView() {
-        
     }
-    
-
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 0
-//    }
 }
