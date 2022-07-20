85298044ff71b025de2648b89dfc253c3a6bbb89
金田岳大 <kanedtakahiro@kanedatakeshidainoMacBook-Air.local>
2022-07-20 19:20:57 +0900

    セルタップで画面遷移

--- SurroundingRestaurantsOfNPB/Controller/RestaurantInfoViewController.swift
+++ SurroundingRestaurantsOfNPB/Controller/RestaurantInfoViewController.swift
@@ -10,27 +10,25 @@ import UIKit
 final class RestaurantInfoViewController: UIViewController {
     
     @IBOutlet private weak var restaurantTableView: UITableView!{
-     didSet {
-        let nib = UINib(nibName: RestaurantInfoCell.className, bundle: nil)
-        restaurantTableView.register(nib, forCellReuseIdentifier: "RestaurantInfoCell")
-        print("✊")
-        restaurantTableView.dataSource = self
-     }
+        didSet {
+            let nib = UINib(nibName: RestaurantInfoCell.className, bundle: nil)
+            restaurantTableView.register(nib, forCellReuseIdentifier: "RestaurantInfoCell")
+            restaurantTableView.dataSource = self
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 }
+
 extension RestaurantInfoViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         15
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = restaurantTableView.dequeueReusableCell(withIdentifier: RestaurantInfoCell.className, for: indexPath) as? RestaurantInfoCell else { return UITableViewCell()
-        }
+        guard let cell = restaurantTableView.dequeueReusableCell(withIdentifier: RestaurantInfoCell.className, for: indexPath) as? RestaurantInfoCell else { return UITableViewCell() }
         return cell
     }
 }
