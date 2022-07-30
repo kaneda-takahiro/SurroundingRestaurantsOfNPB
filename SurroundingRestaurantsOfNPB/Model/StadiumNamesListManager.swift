cce87faad5a218115fd22308998d7370c4869140
金田岳大 <kanedtakahiro@kanedatakeshidainoMacBook-Air.local>
2022-07-30 17:06:44 +0900

    シングルトンパターン実装2スペースミス修正した

--- SurroundingRestaurantsOfNPB/Model/StadiumNamesListManager.swift
+++ SurroundingRestaurantsOfNPB/Model/StadiumNamesListManager.swift
@@ -9,9 +9,7 @@ import Foundation
 
 final class StadiumNamesListManager {
     private init (){}
-    
     private let list = ["ZOZOマリンスタジアム", "東京ドーム", "ベルーナドーム"]
-    
     static let shared = StadiumNamesListManager()
     
     func listCountNumber() -> Int {
